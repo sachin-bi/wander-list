a0d657a3cccfea8c841506e73bd95bb72674b4f8
Sachinandan <sachinandanp5@gmail.com>
2024-06-05 13:40:51 +0530

    styling show listing

--- models/listing.js
+++ models/listing.js
@@ -12,7 +12,7 @@ const listingSchema = new Schema({
     },
     image: {
         type: String,
-        set : (v) => v === "" ? "https://images.unsplash.com/photo-1436259335948-d404e293e2b8?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" : v,
+        set : (v) => v === "" ? "https://images.unsplash.com/photo-1586810724476-c294fb7ac01b?q=80&w=1936&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" : v,
 
     },
     price: Number,
